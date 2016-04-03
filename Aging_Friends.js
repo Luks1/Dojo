d1c18cdef4186cfd14898b112e2174e1a6f7a86a
luxolo <luxolo@projectcodex.co>
2016-04-03 14:44:45 +0200

    my work

--- Aging_Friends.js
+++ Aging_Friends.js
@@ -1,7 +1,21 @@
+
+
 var myStrings = "Xola is 23, Peter is 18, Oya is 29, Ellie is 20, Sive is 27";
 
 //spliting myStrings first automatically goes to array also
-var strngs = myStrings.split(" ,");
+var strings = myStrings.split(",");
 
-//logging out my strings will be on array
-    console.log(strngs);
+
+    console.log(myStrings);
+
+var fixedStrngs = myStrings
+.replace(/is/g, ":");
+console.log(fixedStrngs);
+
+ secondString = [];
+
+ strings.forEach(function(names){
+   secondString.push(names.split("is"));
+
+});
+console.log(secondString);
